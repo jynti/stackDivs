522d8a22408d5204b22a139fbb47d12728a82748
jynti <33417806+jynti@users.noreply.github.com>
2017-12-20 18:12:59 +0530

    Add files via upload

---
+++ stackDivs/js/demo.js
@@ -0,0 +1,49 @@
+function StackDivs(domElements){
+  this.container = domElements.container;
+  this.addButton = domElements.addButton;
+  this.count = 0;
+}
+
+StackDivs.prototype.init = function(){
+  // this.container.addClass("container");
+  this.bindClickToButton();
+}
+
+StackDivs.prototype.bindClickToButton = function(){
+  var _this = this;
+  this.addButton.on("click", function(){
+    var addDiv = $("<div></div>");
+    addDiv.addClass("simple-div");
+    addDiv.text(++(_this.count));
+    _this.container.append(addDiv);
+    // _this.container.on("click", "div", function(event){
+      // var $this = $(this);
+      // $this.addClass("clickedDiv");
+      // if($this.is(":last-child")){
+      //   $this.remove();
+      //   _this.count--;
+      // }
+
+      // console.log($(this));
+
+    // });
+  });
+
+  this.container.on("click", "div", function(){
+    var $this = $(this);
+      $this.addClass("clicked-div");
+      if($this.is(":last-child")){
+        $this.remove();
+        _this.count--;
+      }
+  });
+}
+
+$(document).ready(function(){
+  var domElements = {
+    container: $("div[data-type='container']"),
+    addButton: $("input[data-type='button']")
+  }
+  var stackDivs = new StackDivs(domElements);
+  stackDivs.init();
+});
